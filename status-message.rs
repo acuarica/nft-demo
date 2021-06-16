c66fb1799c463bcd6bcd30a2c816a2a459ee0414
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-06-16 11:04:26 +0200

    Add code snippets for presentation

---
+++ status-message.rs
@@ -0,0 +1,19 @@
+use near_sdk::{near_bindgen, env};
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct StatusMessage {
+    records: HashMap<String, String>,
+}
+
+#[near_bindgen]
+impl StatusMessage {
+    pub fn set_status(&mut self, message: String) {
+        let account_id = env::signer_account_id();
+        self.records.insert(account_id, message);
+    }
+
+    pub fn get_status(&self, account_id: String) -> Option<String> {
+        self.records.get(&account_id).cloned()
+    }
+}
